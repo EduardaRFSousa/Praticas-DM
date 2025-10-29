6c4f01a3efc481246ef62cc04029fbe30a4344b1
LarissamnLira <jfj2@discente.ifpe.edu.br>
2025-10-29 11:36:20 -0300

    Prática 1 - Finalizada

---
+++ WeatherApp/app/src/main/java/com/example/weatherapp/RegisterActivity.kt
@@ -0,0 +1,129 @@
+package com.example.weatherapp
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class RegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                WeatherAppTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        RegisterPage(modifier = Modifier.padding(innerPadding))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RegisterPage(modifier: Modifier = Modifier) {
+    var name by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var confirmpassword by rememberSaveable { mutableStateOf("") }
+    val context = LocalContext.current
+    val activity = context as? Activity
+
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            text = "Faça seu Cadastro:",
+            fontSize = 24.sp
+        )
+
+        OutlinedTextField(
+            value = name,
+            label = { Text(text = "Digite seu nome") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { name = it }
+        )
+
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { email = it }
+        )
+
+        Spacer(modifier = Modifier.size(16.dp)) // Espaçador
+
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        OutlinedTextField(
+            value = confirmpassword,
+            label = { Text(text = "Confirme sua senha") },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f),
+            // ✅ CORRIGIDO: Atribuindo o valor a 'confirmpassword'
+            onValueChange = { confirmpassword = it },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Row(modifier = Modifier) {
+            Button( onClick = {
+                Toast.makeText(activity, "Registro OK!", Toast.LENGTH_LONG).show()
+                activity?.finish()
+
+            },
+                enabled = email.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty() && confirmpassword.isNotEmpty() && password == confirmpassword
+            ) {
+                Text("Registrar")
+            }
+
+            Spacer(modifier = Modifier.size(8.dp))
+
+            Button(
+                onClick = {
+                    name = ""
+                    email = ""
+                    password = ""
+                    confirmpassword = ""
+                }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
