03aa32a0e6511a001092e50c0383b863fbab88f8
Guilherme-Paradiso <guiparadiso2006@gmail.com>
2025-11-18 08:33:44 -0300

    Pratica 4 feita

---
+++ Pratica4/app/src/main/java/com/example/weatherapp/LoginActivity.kt
@@ -0,0 +1,115 @@
+package com.example.weatherapp
+
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginPage(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginPage(modifier: Modifier = Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    val context = LocalContext.current
+    val activity = context as? Activity
+
+    Column(
+        modifier = modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center, // Passo 6
+        horizontalAlignment = CenterHorizontally // Passo 6
+    ) {
+        Text(
+            text = "Faça seu LogIn:",
+            fontSize = 24.sp
+        )
+        Spacer(modifier = Modifier.size(24.dp))
+
+        OutlinedTextField(
+            value = email,
+            label = { Text(text = "Digite seu e-mail") },
+            onValueChange = { email = it },
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f)
+        )
+
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Digite sua senha") },
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(fraction = 0.9f)
+        )
+
+        Spacer(modifier = Modifier.size(24.dp))
+        Row(modifier = modifier) {
+            Button( onClick = {
+                activity?.startActivity(
+                    Intent(activity, MainActivity::class.java).setFlags(FLAG_ACTIVITY_SINGLE_TOP)
+                )
+                Toast.makeText(activity, "Login OK!", Toast.LENGTH_LONG).show()
+
+            }, enabled = email.isNotEmpty() && password.isNotEmpty()) {
+                Text("Login")
+            }
+
+            Button(
+                onClick = {
+                    activity?.startActivity(
+                        Intent(activity, RegisterActivity::class.java)
+                    )
+                }
+            ) {
+                Text("Cadastro")
+            }
+
+            Button(
+                onClick = { email = ""; password = "" }
+            ) {
+                Text("Limpar")
+            }
+        }
+    }
+}
+
